d9dda117ba1bf3482b2d5346ed009f17420c6548
Segun Adeleke <oadeleke123@gmail.com>
2023-08-01 20:37:56 +0100

    add: filter log with regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,3 @@
 #!/usr/bin/env ruby
 # checks for matching cases
-from=ARGV[0].scan(/(from:[a-zA-Z0-9]*)/).join
-to=ARGV[0].scan(/(to:[a-zA-Z0-9+]*)/).join
-flags=ARGV[0].scan(/(flags:[0-9:-]*)/).join
-
-$from, $to, $flags
+puts ARGV[0].scan(/\[from:([a-zA-Z0-9+]*)\] \[to:([a-zA-Z0-9+]*)\] \[flags:([0-9\-:]*)\]/).join(",")
