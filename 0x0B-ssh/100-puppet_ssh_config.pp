e119bc56c20c8c08eb876a18339115c04d046ac3
Segun Adeleke <oadeleke123@gmail.com>
2023-09-24 15:53:39 +0100

    Add: puppet file to set configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+# set ssh config file to use private key and not authenticate password
+exec { 'set_ssh_config':
+   command => '/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+}
+
+exec { 'set_ssh_config2':
+   command => '/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+}
---
+++ 0x0B-ssh/puppet_install_file
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get install -y ruby=1:2.7+1 --allow-downgrades
+apt-get install -y ruby-augeas
+apt-get install -y ruby-shadow
+apt-get install -y puppet
