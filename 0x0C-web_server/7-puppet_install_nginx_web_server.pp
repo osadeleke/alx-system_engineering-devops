2442d2979a2847ad816c5d731ed909b19aaef378
Segun Adeleke <oadeleke123@gmail.com>
2023-09-27 14:14:24 +0100

    Add: commands

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,17 @@
 # install and configure Nginx
-package 'nginx' {
+package { 'nginx'
   ensure => '1.18.0',
   source => 'apt-get',
 }
 
-# set firewall to listening on port 80
+exec { 'set_hello_world'
+  command => 'echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html'
+}
+
+exec { 'set_redirect'
+  command => "sudo sed -i '\\#root /var/www/html;#a\\\\tlocation /redirect_me/ {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}' /etc/nginx/sites-available/default",
+}
+
+exec { 'restart nginx'
+  command => 'sudo service nginx restart',
+}
