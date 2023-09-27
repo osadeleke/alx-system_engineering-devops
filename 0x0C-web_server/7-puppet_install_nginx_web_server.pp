12625beb7b80bba28be04fb549976601ba990efd
Segun Adeleke <oadeleke123@gmail.com>
2023-09-27 14:28:28 +0100

    Fix: add colon at end of name

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,16 +1,19 @@
 # install and configure Nginx
-package { 'nginx'
+package { 'nginx':
   ensure => '1.18.0',
 }
 
-exec { 'set_hello_world'
+exec { 'set_hello_world':
   command => 'echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html'
+  require => Package['nginx'],
 }
 
-exec { 'set_redirect'
+exec { 'set_redirect':
   command => "sudo sed -i '\\#root /var/www/html;#a\\\\tlocation /redirect_me/ {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}' /etc/nginx/sites-available/default",
+  require => require => Package['nginx'],
 }
 
-exec { 'restart nginx'
+exec { 'restart nginx':
   command => 'sudo service nginx restart',
+  require => Exec['set_redirect'],
 }
