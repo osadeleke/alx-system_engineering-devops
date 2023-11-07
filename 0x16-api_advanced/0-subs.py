c9aa8a68f97de41ce95c40b86440cddf9580b5d7
Segun Adeleke <oadeleke123@gmail.com>
2023-11-07 17:17:19 +0100

    queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    # Define the URL of the Reddit API endpoint for subreddit about
+    about_url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+
+    # Send a GET request to the endpoint
+    response = requests.get(about_url)
+
+    # Check if the request was successful (HTTP status code 200)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return int(data['data']['subscribers'])
+    else:
+        return 0
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,1 +1,2 @@
 ### api Advanced
+- **0-subs.py:** A function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
