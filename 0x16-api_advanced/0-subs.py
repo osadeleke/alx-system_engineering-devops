7fc8bec1747808e0bfcf7faa5f197fc1a0e5a401
Segun Adeleke <oadeleke123@gmail.com>
2023-11-09 17:36:03 +0100

    Fix: add user agent header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,17 +9,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-
     # Define the URL of the Reddit API endpoint for subreddit about
     about_url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
 
     # Send a GET request to the endpoint
-    response = requests.get(about_url)
+    response = requests.get(about_url, headers={'User-Agent': 'YourApp/1.0'})
 
     # Check if the request was successful (HTTP status code 200)
     if response.status_code == 200:
         data = response.json()
-        print(data)
         return int(data['data']['subscribers'])
     else:
         return 0
