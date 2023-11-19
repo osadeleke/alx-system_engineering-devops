e1aea804d30ba9bc50028753c30e1bd1eaa7a0f3
Segun Adeleke <oadeleke123@gmail.com>
2023-11-19 02:06:22 +0100

    Add: puppet to fix 500 server error on wordpress site

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,18 @@
+file { '/var/www/html/wp-settings.php':
+  ensure => file,
+}
+
+exec { 'find-class':
+  command => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www
+w/html/wp-settings.php",
+  path    => '/bin:/usr/bin', # Specify the path to the 'sed' command
+  require => File['/var/www/html/wp-settings.php'], # Ensure the file exists beff
+ore running the exec
+}
+
+service { 'apache2':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/var/www/html/wp-settings.php'], # Restart when the Apache  
+configuration file changes
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,3 @@
+### Web Stack Debugging 3
+
+**0-strace_is_your_friend.pp** - fixes automatically a 500 server error on get request to the wordpress website to a wrongly spelled file path within wp-settings.php.
