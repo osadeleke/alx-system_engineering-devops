7e8011732ce74136e04255028d1b70af4cc9b7e7
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2024-02-13 18:42:36 +0100

    Add: api request + gitignore

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module for number of subscribers for subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    number of subscribers for subreddit
+    """
+    results = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers={'User-agent': 'your bot 0.1'})
+
+    if results.status_code == 200:
+        return results.json().get('data').get('subscribers')
+    else:
+        return 0
