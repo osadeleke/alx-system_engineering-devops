c5175f91aa5f1b33c3072d40ff6ccc92d0ba9a34
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2024-02-14 10:22:39 +0100

    add: top ten api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,13 +12,10 @@ def top_ten(subreddit):
     results = requests.get(
         f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
         headers={"User-Agent": "my bot 0.1"})
-    response_data = results.json()
-    # if results.status_code != 200:
-    #     print(None)
-    # else:
-    #     for i in range(10):
-    #         print(response_data['data']['children'][i]['data']['title'])
-    titles = response_data['data']['children']
-    print(titles)
-
-    # print(response_data)
+    if results.status_code == 200:
+        response_data = results.json()
+        titles = response_data['data']['children']
+        for title in titles:
+            print(title['data']['title'])
+    else:
+        print(None)
